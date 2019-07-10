df851f9c6b574defbbf66bfe86b7540123b92337
igorkajtez <kajtezigor@gmail.com>
2019-07-10 13:23:07 +0200

    updated Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,34 @@
 package main;
 
-import java.net.URL;
+import controller.MainMenuController;
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenuView.fxml"));
+        MainMenuController menuController = new MainMenuController();
+        loader.setController(menuController);        
+        
+        Parent root = loader.load();
+        menuController.setStage(primaryStage);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("view/style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        URL fxmlUrl = getClass().getClassLoader().getResource("view/GameView.fxml");
-        Parent root = FXMLLoader.load(fxmlUrl);
-        
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
 }
