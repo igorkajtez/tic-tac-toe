bbdcc241966be4fc5b85198ebb16dde9207ad846
igorkajtez <kajtezigor@gmail.com>
2019-07-10 13:15:43 +0200

    added ITile interface

---
+++ src/model/ITile.java
@@ -0,0 +1,10 @@
+package model;
+
+
+public interface ITile {
+    String getSymbol();
+    void setSymbol(String symbol);
+    void setTileWidth(double width);
+    void setTileHeight(double height);
+    
+}
